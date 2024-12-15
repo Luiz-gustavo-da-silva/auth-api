d2deddaad03fbb9a6a2d435c14ca1375271e89f6
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2024-12-15 13:29:55 -0300

    feat: ajuste nos arquivos

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const http = require('http');
-const app = require('./index');
+const app = require('../index');
 
 const server = http.createServer(app);
 server.listen(process.env.PORT)
